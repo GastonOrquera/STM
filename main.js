7ee40d260ff7f346b27a68ca7d230186e099c2b3
GastonOrquera <gastonorquera29@gmail.com>
2024-02-07 04:30:45 -0300

    s-t-m

--- main.js
+++ main.js
@@ -13,6 +13,48 @@ hamburger.addEventListener('click', function () {
         link.addEventListener('click', function () {
             hamburger.classList.remove('active');
             nav.classList.remove('active');
+
         });
     });
+});
+
+
+// SCROLL REVEAL PARA TITULOS
+
+ScrollReveal().reveal('.scroll-revealTitles', {
+    origin: 'top',
+    distance: '40px',
+    duration: 1000,
+    easing: 'cubic-bezier(0.5,0,0,1)',
+    interval: 200
+});
+
+// SCROLL REVEAL PARA PARRAFOS
+
+ScrollReveal().reveal('.scroll-revealP', {
+    origin: 'left',
+    distance: '30px',
+    duration: 600,
+    esasing: 'cubic-bezier(0.5,0,0,1)',
+    interval: 400
+});
+
+// SCROLL REVEAL PARA IMAGENES
+
+ScrollReveal().reveal('.scroll-revealImgs', {
+    origin: 'right',
+    distance: '20px',
+    duration: 2000,
+    easing: 'cubic-bezier(0.5,0,0,1)',
+    interval: 600
+});
+
+// SCROLL REVEAL PARA LI
+
+ScrollReveal().reveal('.scroll-revealLi', {
+    origin: 'left',
+    distance: '20px',
+    duration: 3000,
+    easing: 'cubic-bezier(0.5,0,0,1)',
+    interval: 600
 });
