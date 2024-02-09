e64d21c644449ef2d37c5758d2f7cacc00d0715c
GastonOrquera <gastonorquera29@gmail.com>
2024-02-09 19:55:24 -0300

    s-t-m

--- main.js
+++ main.js
@@ -4,20 +4,16 @@ let nav = document.querySelector('nav');
 
 let navLinks = document.querySelectorAll('nav a');
 
-
 hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('active');
     nav.classList.toggle('active');
-
     navLinks.forEach(function (link) {
         link.addEventListener('click', function () {
             hamburger.classList.remove('active');
             nav.classList.remove('active');
-
         });
     });
 });
-
 
 // SCROLL REVEAL PARA TITULOS
 
